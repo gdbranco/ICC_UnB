8b1f00ec3716b4b80ff181290b88d45e5edf2b96
Guilherme David Branco <gdbranco@gmail.com>
2018-09-03 22:11:00 -0300

    Fix ex5 l2

--- Listas/2/ex5.py
+++ Listas/2/ex5.py
@@ -3,7 +3,7 @@ m, d = (int(x) for x in input().split())
 meses = [-1,31,28,31,30,31,30,31,31,30,31,30,31]
 s = 1
 for _ in range(1, meses[m]):
-if(d%7 == 0):
-    s += 1
-d+=1
+    if(d%7 == 0):
+        s += 1
+    d+=1
 print(s)
